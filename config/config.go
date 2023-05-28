704171da86c52333ae2ed37f7fc651caa46e0526
winai-pgm-itsystem <winai.k@itsystem-igt.com>
2023-05-28 10:41:07 +0700

    set config

--- config/config.go
+++ config/config.go
@@ -1,21 +1,14 @@
 package config
 
 import (
+	"fmt"
 	"log"
-	"os"
+	"math"
 	"strconv"
 	"time"
 
 	"github.com/joho/godotenv"
 )
-
-func envPath() string {
-	if len(os.Args) == 1 {
-		return ".env"
-	} else {
-		return os.Args[1]
-	}
-}
 
 func LoadConfig(path string) IConfig {
 
@@ -37,16 +30,76 @@ func LoadConfig(path string) IConfig {
 			}(),
 			name:    envMap["APP_NAME"],
 			version: envMap["APP_VERSION"],
-			readTimeOut: time.Duration(func() int {
-				r, err := strconv.Atoi(envMap["APP_READ_TIMEOUT"])
+			readTimeout: func() time.Duration {
+				t, err := strconv.Atoi(envMap["APP_READ_TIMEOUT"])
 				if err != nil {
 					log.Fatalf("failed to convert read time out: %v", err)
 				}
-				return r
-			}()),
+				return time.Duration(int64(t) * int64(math.Pow10(9)))
+			}(),
+			writeTimeout: func() time.Duration {
+				t, err := strconv.Atoi(envMap["APP_WRITE_TIMEOUT"])
+				if err != nil {
+					log.Fatalf("failed to convert write time out: %v", err)
+				}
+				return time.Duration(int64(t) * int64(math.Pow10(9)))
+			}(), bodyLimit: func() int {
+				b, err := strconv.Atoi(envMap["APP_BODY_LIMIT"])
+				if err != nil {
+					log.Fatalf("failed to convert  body limit: %v", err)
+				}
+				return b
+			}(),
+			fileLimit: func() int {
+				f, err := strconv.Atoi(envMap["APP_FILE_LIMIT"])
+				if err != nil {
+					log.Fatalf("failed to convert  file limit: %v", err)
+				}
+				return f
+			}(),
+			gcpbucket: envMap["APP_GCP_BUCKET"],
 		},
-		db:  &db{},
-		jwt: &jwt{},
+		db: &db{
+			host: envMap["DB_HOST"],
+			port: func() int {
+				p, err := strconv.Atoi(envMap["DB_PORT"])
+				if err != nil {
+					log.Fatalf("failed to convert  db  port: %v", err)
+				}
+				return p
+			}(),
+			protocol: envMap["DB_PROTOCOL"],
+			username: envMap["DB_USERNAME"],
+			password: envMap["DB_PASSWORD"],
+			database: envMap["DD_DATABASE"],
+			sslMode:  envMap["DB_SSL_MODE"],
+			maxConnections: func() int {
+				m, err := strconv.Atoi(envMap["DB_MAX_CONNECTIONS"])
+				if err != nil {
+					log.Fatalf("failed to convert max connections : %v", err)
+				}
+				return m
+			}(),
+		},
+		jwt: &jwt{
+			adminKey:  envMap["JWT_ADMIN_KEY"],
+			secretKey: envMap["JWT_SECRET_KEY"],
+			apiKey:    envMap["JWT_API_KEY"],
+			accessExpiresAt: func() int {
+				t, err := strconv.Atoi(envMap["JWT_ACCESS_EXPIRES"])
+				if err != nil {
+					log.Fatalf("failed to convert  db  port: %v", err)
+				}
+				return t
+			}(),
+			refreshExpiresAt: func() int {
+				t, err := strconv.Atoi(envMap["JWT_REFRESH_EXPIRES"])
+				if err != nil {
+					log.Fatalf("failed to convert  db  port: %v", err)
+				}
+				return t
+			}(),
+		},
 	}
 }
 
@@ -63,6 +116,16 @@ type config struct {
 }
 
 type IAppConfig interface {
+	Url() string // host:port
+	Name() string
+	Version() string
+	ReadTimeout() time.Duration
+	WriteTimeout() time.Duration
+	BodyLimit() int
+	FileLimit() int
+	GCPBucket() string
+	Host() string
+	Port() int
 }
 
 type app struct {
@@ -70,18 +133,31 @@ type app struct {
 	port         int
 	name         string
 	version      string
-	readTimeOut  time.Duration
-	writeTimeOut time.Duration
+	readTimeout  time.Duration
+	writeTimeout time.Duration
 	bodyLimit    int //bytes
 	fileLimit    int //bytes
-	gcp          string
+	gcpbucket    string
 }
 
 func (c *config) App() IAppConfig {
-	return nil
+	return c.app
 }
 
+func (a *app) Url() string                 { return fmt.Sprintf("%s:%d", a.host, a.port) } // host:port
+func (a *app) Name() string                { return a.name }
+func (a *app) Version() string             { return a.version }
+func (a *app) ReadTimeout() time.Duration  { return a.readTimeout }
+func (a *app) WriteTimeout() time.Duration { return a.writeTimeout }
+func (a *app) BodyLimit() int              { return a.bodyLimit }
+func (a *app) FileLimit() int              { return a.fileLimit }
+func (a *app) GCPBucket() string           { return a.gcpbucket }
+func (a *app) Host() string                { return a.host }
+func (a *app) Port() int                   { return a.port }
+
 type IDbConfig interface {
+	Url() string
+	MaxOpenConns() int
 }
 
 type db struct {
@@ -96,10 +172,30 @@ type db struct {
 }
 
 func (c *config) Db() IDbConfig {
-	return nil
+	return c.db
 }
 
+func (d *db) Url() string {
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		d.host,
+		d.port,
+		d.username,
+		d.password,
+		d.database,
+		d.sslMode,
+	)
+}
+func (d *db) MaxOpenConns() int { return d.maxConnections }
+
 type IJwtConfig interface {
+	SecretKey() []byte
+	AdminKey() []byte
+	ApiKey() []byte
+	AccessExpiresAt() int
+	RefreshExpiresAt() int
+	SetJwtAccessExpires(t int)
+	SetJwtRefreshExpires(t int)
 }
 
 type jwt struct {
@@ -111,5 +207,13 @@ type jwt struct {
 }
 
 func (c *config) Jwt() IJwtConfig {
-	return nil
+	return c.jwt
 }
+
+func (j *jwt) SecretKey() []byte          { return []byte(j.secretKey) }
+func (j *jwt) AdminKey() []byte           { return []byte(j.adminKey) }
+func (j *jwt) ApiKey() []byte             { return []byte(j.apiKey) }
+func (j *jwt) AccessExpiresAt() int       { return j.accessExpiresAt }
+func (j *jwt) RefreshExpiresAt() int      { return j.refreshExpiresAt }
+func (j *jwt) SetJwtAccessExpires(t int)  { j.accessExpiresAt = t }
+func (j *jwt) SetJwtRefreshExpires(t int) { j.refreshExpiresAt = t }
--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/winai-pgm-itsystem/all-day-shop/config"
+)
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
+func main() {
+	cfg := config.LoadConfig(envPath())
+	fmt.Println(cfg.App())
+
+}
